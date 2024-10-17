9476b5fa66baaabc2da1246a3b478ecf155daf26
Krystle <krystle@Krystles-MBP.lan>
2024-10-16 19:23:28 -0700

    Can solve coding challenges

--- algorithms/two-sum.js
+++ algorithms/two-sum.js
@@ -1,1 +1,18 @@
+function twoSum(nums, target) {
+    const numMap = new Map()
 
+    //loop through the array
+    for(let i = 0; i < nums.length; i++){
+        const complement = target - nums[i];
+
+        if (numMap.has(complement)) {
+            return [numMap.get(complement)]
+        }
+        numMap.set(nums[i], i)
+    }
+    return []
+}
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target)); 
